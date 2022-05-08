2e178d227409e2ec9ed15d33a1d6390b49f9b3f8
Sandipan Nath <sn455@cornell.edu>
2022-05-08 15:18:17 -0400

    import react-router-dom

--- src/App.js
+++ src/App.js
@@ -1,28 +1,18 @@
 import logo from './logo.svg';
 import './App.css';
+import React, { useEffect } from 'react'
+import { Routes, Route, useNavigate } from 'react-router-dom'
 
-function App() {
+import Home from './container/Home'
+
+const App = () => {
+  const navigate = useNavigate();
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload. Test. Tailwind.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-        <h1 className="text-3xl font-bold underline">
-          Hello world!
-        </h1>
-      </header>
-    </div>
-  );
+    <Routes>
+      <Route path="/*" element={<Home/>} />
+    </Routes>
+  )
 }
 
 export default App;
