555b4a4dd7efb157a58d4c6c5ba01a10d414f658
Sandipan Nath <sn455@cornell.edu>
2022-05-08 15:42:57 -0400

    fixed build script

--- src/components/Spinner.jsx
+++ src/components/Spinner.jsx
@@ -1,16 +1,8 @@
 import React from 'react';
-import ClipLoader from 'react-spinners/ClipLoader';
 
 function Spinner({ message }) {
   return (
     <div className="flex flex-col justify-center items-center w-full h-full">
-      <ClipLoader
-        color="#00BFFF"
-        height={50}
-        width={200}
-        className="m-5"
-      />
-
       <p className="text-lg text-center px-2">{message}</p>
     </div>
   );
